6d12e962d3e07f060a94c6f2bc682534fec23eb0
teko teko <tekosolution23@gmail.com>
2024-01-24 13:37:06 +0100

    delete comments

--- src/Controllers/promocodeController.js
+++ src/Controllers/promocodeController.js
@@ -17,57 +17,8 @@ module.exports.promo = async (req, res,) => {
     }
 }
 
-// let gethome = async() => {
-//     // rest param (...param) prend des valeurs libre et les places dans un tableau 
-    
-//     //const tabNumbers = [5, 8, 21, 46, 17]
-
-//     const sumTabNumber = (...n) => {
-//         console.log("REST PARAM -------> : ", n);
-//         let result = 0;
-//         for(const num of n) {
-//             result += num
-//         }
-//         return result;
-//     }
-
-//     return sumTabNumber(5, 8, 21, 46, 17);
-// }
-// let getAppart = async() => {
-//     // opérateur spread  (...[5, 8, 12]) prend des valeurs d'un tableau et les transforme en nombre libre 
-
-//     const tabNumbers = [5, 8, 21, 46, 17]
-
-//     const sumTabNumber = (n1, n2, n3) => {
-//         return n1 + n2 + n3;
-//     }
-
-//     return sumTabNumber(...tabNumbers);
-// }
-
-// const learnJsSpread = async() => {
-//     return new Promise((resolve, reject) => {
-//         // on peut aussi utiliser les spread pour fusionner plusieurs tableau 
-
-//         const redFruits =  ["fraise", "framboise"];
-//         const greenFruits =  ["pomme", "poire"];
-
-//         if(redFruits && greenFruits) {
-//             const fruits = redFruits && redFruits.length > 0 || greenFruits && greenFruits.length > 0 ? [...redFruits, ...greenFruits] : "undefinded"
-//             resolve(fruits)
-
-//         } else {
-//             reject(new Error("ERROR"))
-//         }
-
-//     });
-// }
-
 
 module.exports.readPromo = async (req, res) => {
-    // console.log("ASYNC1 -----> : ", await gethome());
-    // console.log("ASYNC2 -----> : ", await getAppart());
-    // console.log("ASYNC3 -----> : ", await learnJsSpread());
     const  redeemInfo = req.body;
     try{
         const promoCode = await checkPromo(redeemInfo.promocode_name, redeemInfo.code);
