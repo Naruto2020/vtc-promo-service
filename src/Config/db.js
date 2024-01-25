ee240e929ede4d41c61d7dbd5f888bb2390c45d6
teko teko <tekosolution23@gmail.com>
2024-01-25 16:37:37 +0100

    refactoring 2

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 // connct to MongoDB data base
-//MONGODB_URI = process.env.myDbUrl
-MONGODB_URI = "mongodb+srv://steve:Kmia%400703@cluster0.5hvbw.mongodb.net/vtc"
+MONGODB_URI = process.env.myDbUrl
+
 
 mongoose.connect(
     MONGODB_URI , 
